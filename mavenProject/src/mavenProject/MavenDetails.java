24a0706540ab4ea88d9f49e7c6c21ed90e1be253
kbagisetty <kbagisetty@OptiplexKIRAN>
2018-01-26 10:05:10 -0400

    Added print statement

---
+++ mavenProject/src/mavenProject/MavenDetails.java
@@ -0,0 +1,43 @@
+package mavenProject;
+
+/* What is Maven?
+ * Maven is a tool from Apache for building and managing Java projects. 
+ * Maven works based on its well defined project object model (POM)
+ * Maven can manage building, reporting and documentation of a java project
+ * 
+ * Why Maven?
+ * Simple project setup
+ * Well defined project's directory structure
+ * integrating testing in the build cycle
+ * Maven's huge plugin library
+ * Manages dependencies very well and reuses
+ * 
+ * How to use Maven?
+ * Jave should be installed and the environment variables set
+ * Eclipse should installed
+ * 
+ * Maven resources?
+ * maven.apache.org
+ * https://maven.apache.org/download.cgi
+ * create env variable for maven 
+ * 
+ */
+
+public class MavenDetails {
+
+	public static void main(String[] args) {
+		displayGreetings();
+		
+
+	}
+
+	private static void displayGreetings() {
+		// TODO Auto-generated method stub
+		System.out.println("Learning integrating GitHub");
+		System.out.println("making changes");
+		
+	}
+	
+
+
+}
