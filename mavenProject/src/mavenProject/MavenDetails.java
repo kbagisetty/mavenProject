23fcd835613977966abf5f39793dda5ab0daace1
kbagisetty <kbagisetty@OptiplexKIRAN>
2018-01-26 13:35:45 -0400

    Added Maven details methods

--- mavenProject/src/mavenProject/MavenDetails.java
+++ mavenProject/src/mavenProject/MavenDetails.java
@@ -4,31 +4,53 @@ package mavenProject;
  * Maven is a tool from Apache for building and managing Java projects. 
  * Maven works based on its well defined project object model (POM)
  * Maven can manage building, reporting and documentation of a java project
- * 
- * Why Maven?
- * Simple project setup
- * Well defined project's directory structure
- * integrating testing in the build cycle
- * Maven's huge plugin library
- * Manages dependencies very well and reuses
- * 
- * How to use Maven?
- * Jave should be installed and the environment variables set
- * Eclipse should installed
- * 
- * Maven resources?
- * maven.apache.org
- * https://maven.apache.org/download.cgi
- * create env variable for maven 
- * 
+
  */
 
 public class MavenDetails {
 
 	public static void main(String[] args) {
 		displayGreetings();
+		mavenIs();
+		whyMaven();
+		howToUseMaven();
+		mavenResources();
+		
 		
 
+	}
+
+	private static void mavenResources() {
+		// TODO Auto-generated method stub
+		System.out.println(" * Maven resources?\r\n" + 
+				" * maven.apache.org\r\n" + 
+				" * https://maven.apache.org/download.cgi\r\n" + 
+				" * create env variable for maven ");
+	}
+
+	private static void howToUseMaven() {
+		// TODO Auto-generated method stub
+		System.out.println(" * How to use Maven?\r\n" + 
+				" * Jave should be installed and the environment variables set\r\n" + 
+				" * Eclipse should installed");
+	}
+
+	private static void whyMaven() {
+		// TODO Auto-generated method stub
+		System.out.println(" * Why Maven?\r\n" + 
+				" * Simple project setup\r\n" + 
+				" * Well defined project's directory structure\r\n" + 
+				" * integrating testing in the build cycle\r\n" + 
+				" * Maven's huge plugin library\r\n" + 
+				" * Manages dependencies very well and reuses");
+	}
+
+	private static void mavenIs() {
+		// TODO Auto-generated method stub
+		System.out.println("/* What is Maven?\r\n" + 
+				" * Maven is a tool from Apache for building and managing Java projects. \r\n" + 
+				" * Maven works based on its well defined project object model (POM)\r\n" + 
+				" * Maven can manage building, reporting and documentation of a java project");
 	}
 
 	private static void displayGreetings() {
@@ -38,6 +60,8 @@ public class MavenDetails {
 		System.out.println("making another change");
 		
 	}
+	
+	
 	
 
 
